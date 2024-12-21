8edbf4ef835038aabe8047f301421f49225cf768
Nikita <onishenkonikitos@gmail.com>
2024-12-21 05:07:38 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const array = [
+    ...document.querySelectorAll("input")
+];
+array.forEach((element)=>{
+    const label = document.createElement("label");
+    const inputName = element.name.charAt(0).toUpperCase() + element.name.slice(1);
+    label.classList.add("field-label");
+    label.setAttribute("for", element.id);
+    label.textContent = inputName;
+    element.parentNode.insertBefore(label, element);
+    element.setAttribute("placeholder", inputName);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
